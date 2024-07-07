b3c1b366c5b7d8438919eb258953286944d131da
leon2049 <320696041@qq.com>
2024-07-07 23:20:56 +0800

    refactor: 使用路由切换页面

--- src/router/index.js
+++ src/router/index.js
@@ -2,60 +2,46 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
-    path: '/homePage',
-    name: 'homepage',
-    component: () => import('@/components/homePage/index.vue'),
-    meta: {
-      title: '首页',
-      keepAlive: true,
-    },
-  },
-  {
-    path: '/produceInfo',
-    name: 'produceInfo',
-    component: () => import('@/components/produceInfo/index.vue'),
-    meta: {
-      title: '产品信息',
-      keepAlive: true,
-    },
-  },
-  {
-    path: '/callUs',
-    name: 'callUs',
-    component: () => import('@/components/callUs/index.vue'),
-    meta: {
-      title: '联系我们',
-      keepAlive: true,
-    },
-  },
-  {
-    path: '/checkMore',
-    name: 'checkMore',
-    component: () => import('@/components/checkMore/index.vue'),
-    meta: {
-      title: '查看更多',
-      keepAlive: true,
-    },
-  },
-  {
-    path: '/burlesqueSociety/index',
+    path: '/burlesqueSociety',
     name: 'index',
     component: () => import('../views/burlesqueSociety/index.vue'),
     meta: {
       title: '滑稽社',
       keepAlive: true, // 需要被缓存
     },
+    children: [
+      {
+        path: 'produceInfo',
+        name: 'produceInfo',
+        component: () => import('@/components/produceInfo/index.vue'),
+      },
+      {
+        path: 'callUs',
+        name: 'callUs',
+        component: () => import('@/components/callUs/index.vue'),
+      },
+      {
+        path: 'checkMore',
+        name: 'checkMore',
+        component: () => import('@/components/checkMore/index.vue'),
+      },
+      {
+        path: 'homePage',
+        name: 'homepage',
+        component: () => import('@/components/homePage/index.vue'),
+      },
+    ],
   },
 
   {
     path: '/',
-    redirect:"/burlesqueSociety/index"
-  }
+    redirect: '/burlesqueSociety/index',
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
 })
 
 export default router
