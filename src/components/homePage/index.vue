c5365fff51171cad6c671e17b3856c2ffdb36e9d
leon2049 <320696041@qq.com>
2024-07-08 09:53:00 +0800

    注释

--- src/components/homePage/index.vue
+++ src/components/homePage/index.vue
@@ -1,16 +1,26 @@
 <template>
-  <div>
-    <el-carousel :interval="5000" arrow="always">
-      <el-carousel-item v-for="item in 4" :key="item">
-        <h3 text="2xl" justify="center">{{ item }}</h3>
-      </el-carousel-item>
-    </el-carousel>
+  <div class="homePageStyle">
+    <div class="backgroundImgTop"></div>
+    <div class="carouselAndDescribeStyle"></div>
+    <div class="circleBlack"></div>
+    <div class="backgroundImgBottom">
+      <view class="footerIndex"> </view>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "homePage",
+  data() {
+    return {
+      // carouselList: [
+      //   { fileUrl: require("https://picsum.photos/200/300"), name: "carousel1" },
+      //   { fileUrl: require("https://picsum.photos/200/300"), name: "carousel2" },
+      //   { fileUrl: require("https://picsum.photos/200/300"), name: "carousel3" },
+      // ],
+    };
+  },
 };
 </script>
 
@@ -23,11 +33,41 @@ export default {
   text-align: center;
 }
 
-.el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
+.backgroundImgTop {
+  width: 100%;
+  height: 45vh;
+  background: linear-gradient(to right, #0074d9, #8a2be2, #ff69b4);
 }
-
-.el-carousel__item:nth-child(2n + 1) {
-  background-color: #d3dce6;
+.carouselAndDescribeStyle {
+  width: 90%;
+  height: 45vh;
+  display: flex;
+  flex-direction: row;
+  background: linear-gradient(to right, #0074d9, #8a2be2);
+  border-radius: 20px;
+  border: 1px solid white;
+  margin-top: -35vh;
+  z-index: 9999;
+}
+.backgroundImgBottom {
+  width: 100%;
+  height: 45vh;
+  background-color: #1b1a1a;
+  position: absolute;
+  margin-top: 45vh;
+}
+.footerIndex {
+  width: 80%;
+  height: 20vh;
+}
+.circleBlack {
+  width: 20%;
+  height: 200px;
+  border-radius: 50%;
+  background-color: #1b1a1a;
+  margin-left: auto;
+  z-index: 9998;
+  margin-top: -12%;
+  margin-right: -5%;
 }
 </style>
