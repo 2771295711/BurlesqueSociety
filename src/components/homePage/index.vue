94cc55a88c21242da1ca803a14f9122d502438ba
leon2049 <320696041@qq.com>
2024-07-08 09:17:47 +0800

    'init'

--- src/components/homePage/index.vue
+++ src/components/homePage/index.vue
@@ -1,19 +1,33 @@
 <template>
-<div class="homePageStyle">
-  homepage
-</div>
+  <div>
+    <el-carousel :interval="5000" arrow="always">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3 text="2xl" justify="center">{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "homePage"
-}
+  name: "homePage",
+};
 </script>
 
 <style scoped>
-.homePageStyle {
-  width: 100%;
-  height: 90vh;
-  background-color: blue;
+.el-carousel__item h3 {
+  color: #475669;
+  opacity: 0.75;
+  line-height: 300px;
+  margin: 0;
+  text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
 }
 </style>
